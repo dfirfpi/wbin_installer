07d8a20600ca802e97bcf808009c3face84aabdd
dfirfpi <francesco.picasso@gmail.com>
2016-08-11 14:35:31 +0200

    working on..

--- wbin_install_manual.sh
+++ wbin_install_manual.sh
@@ -11,6 +11,7 @@
 #
 #-------------------------------------------------------------------
 
+TUXREL="fc23"
 LOG="wbin_install.log"
 ROOT_FOLDER="/wbin"
 INS_FOLDER="$ROOT_FOLDER/install"
@@ -18,12 +19,33 @@ INS_FOLDER="$ROOT_FOLDER/install"
 [ -d ${ROOT_FOLDER} ] || { echo "wbin folder does not exist!"; exit 1; }
 [ -d ${INS_FOLDER} ] || { echo "wbin/install folder does not exist!"; exit 1; }
 
-echo "Starting DFIRFPI tools installation [manual]" > ${LOG}
+ARC_FOLDER="$ROOT_FOLDER/archive"
+WIN_FOLDER="$ROOT_FOLDER/win"
+
+#-------------------------------------------------------------------
+# Function
+
+function install_libyal {
+
+    LIB_NAME=$1
+    LIB_VERSION=$2
+
+    SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+
+    wget -nv -nc "${SOURCE}/${LIB_NAME}-${LIB_VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+    if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${LIB_NAME} ${LIB_VERSION}"; fi
+    wget -nv -nc "${SOURCE}/${LIB_NAME}-tools-${LIB_VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+    if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${LIB_NAME} ${LIB_VERSION} tools"; fi
+    wget -nv -nc "${SOURCE}/${LIB_NAME}-python-${LIB_VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+    if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${LIB_NAME} ${LIB_VERSION} python2"; fi
+    wget -nv -nc "${SOURCE}/${LIB_NAME}-python3-${LIB_VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>$
+    if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${LIB_NAME} ${LIB_VERSION} python3 libs"; fi
+    sudo dnf -y install ${ARC_FOLDER}/${LIB_NAME}-*.rpm >> ${LOG} 2>&1
+}
 
 #-------------------------------------------------------------------
 
-ARC_FOLDER="$ROOT_FOLDER/archive"
-WIN_FOLDER="$ROOT_FOLDER/win"
+echo "Starting DFIRFPI tools installation [manual]" > ${LOG}
 
 # 1st level folders
 [ -d ${ARC_FOLDER} ] || mkdir ${ARC_FOLDER}
@@ -50,33 +72,90 @@ sudo dnf -y install openssl-libs openssl fuse fuse-python >> ${LOG} 2>&1
 
 # SleuthKit section -----------------------------
 sudo dnf -y install afflib afftools >> ${LOG} 2>&1
-EWF_VER="20160424-1"
-wget -Nnv "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/libewf-${EWF_VER}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
-if [[ $? != 0 ]]; then echo "unable to download libewf libraries"; exit 1; fi
-wget -Nnv "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/libewf-tools-${EWF_VER}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
-if [[ $? != 0 ]]; then echo "unable to download libewf libraries"; exit 1; fi
-wget -Nnv "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/libewf-python-${EWF_VER}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
-if [[ $? != 0 ]]; then echo "unable to download libewf libraries"; exit 1; fi
-wget -Nnv "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/libewf-python3-${EWF_VER}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
-if [[ $? != 0 ]]; then echo "unable to download libewf libraries"; exit 1; fi
-sudo dnf -y install ${ARC_FOLDER}/libewf-*.rpm >> ${LOG} 2>&1
+
+install_libyal "libewf" "20160424-1"
 
 TSK_VER="4.3.0-1"
-wget -Nnv "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/sleuthkit-${TSK_VER}.fc23.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+wget -nv -nc "https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/fc23/x86_64/sleuthkit-${TSK_VER}.fc23.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
 if [[ $? != 0 ]]; then echo "unable to download sleuthkit"; exit 1; fi
 sudo dnf -y install ${ARC_FOLDER}/sleuthkit-*.rpm >> ${LOG} 2>&1
 
+NAME="pytsk3-python"
+VERSION="20160721-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${NAME} ${VERSION}"; exit; fi
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}.x86_64.rpm >> ${LOG} 2>&1
+
 # Plaso section ---------------------------------
 
-sudo dnf -y install artifacts bencode binplist  >> ${LOG} 2>&1
+sudo dnf install -y python-dateutil python3-dateutil python-dpkt >> ${LOG} 2>&1
+sudo dnf install -y python-ipython python3-ipython >> ${LOG} 2>&1
+sudo dnf install -y pyparsing python-pefile python-psutil python3-psutil >> ${LOG} 2>&1
+sudo dnf install -y yara yara-python >> ${LOG} 2>&1
 
+sudo pip install artifacts bencode binplist construct dfdatetime >> ${LOG} 2>&1
+sudo pip install hachoir_core hachoir_metadata hachoir_parser >> ${LOG} 2>&1
+sudo pip install efilter xlsxwriter >> ${LOG} 2>&1
+
+install_libyal "libbde"     "20160731-1"
+install_libyal "libesedb"   "20160622-1"
+install_libyal "libevt"     "20160421-1"
+install_libyal "libevtx"    "20160421-1"
+install_libyal "libfsntfs"  "20160418-1"
+install_libyal "libfvde"    "20160801-1"
+install_libyal "libfwsi"    "20160110-1"
+install_libyal "liblnk"     "20160420-1"
+install_libyal "libmsiecf"  "20160421-1"
+install_libyal "libolecf"   "20160423-1"
+install_libyal "libqcow"    "20160123-1"
+install_libyal "libregf"    "20160424-1"
+install_libyal "libscca"    "20160108-1"
+install_libyal "libsigscan" "20160312-1"
+install_libyal "libsmdev"   "20160320-1"
+install_libyal "libsmraw"   "20160424-1"
+install_libyal "libvhdi"    "20160424-1"
+install_libyal "libvmdk"    "20160119-1"
+install_libyal "libvshadow" "20160110-1"
+
+NAME="python-dfwinreg"
+VERSION="20160428-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.noarch.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${NAME} ${VERSION}"; exit; fi
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}*.rpm >> ${LOG} 2>&1
+
+NAME="python-dfvfs"
+VERSION="20160803-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.noarch.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+if [[ $? != 0 ]]; then echo "[ERROR] unable to download ${NAME} ${VERSION}"; exit; fi
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}.x86_64.rpm >> ${LOG} 2>&1
+
+NAME="libyara"
+VERSION="3.5.0-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}.x86_64.rpm >> ${LOG} 2>&1
+
+NAME="yara"
+VERSION="3.5.0-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}.x86_64.rpm >> ${LOG} 2>&1
+
+NAME="yara-python"
+VERSION="3.5.0-1"
+SOURCE="https://github.com/dfirfpi/rpm_bin_dfirfpi/raw/master/${TUXREL}/x86_64"
+wget -nv -nc "${SOURCE}/${NAME}-${VERSION}.x86_64.rpm" -P ${ARC_FOLDER} >> ${LOG} 2>&1
+sudo dnf -y install ${ARC_FOLDER}/${NAME}-${VERSION}.x86_64.rpm >> ${LOG} 2>&1
+
+exit
 
 # NTFS section ----------------------------------
 
 sudo pip install analyzeMFT  >> ${LOG} 2>&1
 [ -L ${NTFS_FOLDER}/analyzeMFT.py ] || ln -s /usr/bin/analyzeMFT.py ${NTFS_FOLDER}/analyzeMFT.py
-
-exit
 
 # RE section ------------------------------------
 
